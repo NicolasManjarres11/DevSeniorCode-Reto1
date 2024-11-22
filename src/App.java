4a1f1bda3427e2923f46bd96797f53294e642937
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2024-11-22 14:09:37 -0500

    Implementación de los métodos de elección de naves en la maqueta inicial del proyecto

--- src/App.java
+++ src/App.java
@@ -2,21 +2,84 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    
-    public static String[] planets = {"Mercurio", "Venus", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"};
-    public static double[] distances = {91.0, 41.0, 78.0, 628.0, 1275.0, 2723.0, 4351.0};
-    
-    public static String[] starships = {"Astra Voyager","Pioneer Horizon","Stellar Odyssey","Cosmos Endeavor",};
-    public static double[] speed = {4.0, 17.0, 35.0 ,55.0}; //Velocidades en millones de km/h
+
+    //Clases a utilizar
 
     static Scanner sc = new Scanner(System.in);
     static Random rnd = new Random();
 
-    public static void main(String[] args) throws Exception {
-        chooseStarship();
+    //Planetas, detalles y descripción
+    
+    public static String[] planets = {"Mercurio", "Venus", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"};
+    public static double[] distances = {91.0, 41.0, 78.0, 628.0, 1275.0, 2723.0, 4351.0};
+    public static String[] descriptionPlanet = {}; //Pendiente por agregar descripción correspondiente
+
+    //Naves espaciales, detalles y descripción
+
+    public static String[] starships = {"Astra Voyager","Pioneer Horizon","Stellar Odyssey","Cosmos Endeavor",};
+    public static double[] speed = {4.0, 17.0, 35.0 ,55.0}; //Velocidades en millones de km/h
+    public static String[] descriptionStarship = {}; //Pendiente por agregar descripción correspondiente
+
+    //Variables a utilizar
+
+    public static int choosePlanet;
+    public static int chooseStarship;
+    public static double fuelReserve;
+    public static double oxigenReserve;
+
+    //Variables lógicas
+
+    static boolean startSimulation = true;
+    
+    public static void main(String[] args){
+        menu(); 
+        sc.close();
     }
 
-    
+
+    //Menú de opciones
+
+    public static void menu(){
+
+        int option;
+
+        System.out.println("Simulador de Viaje interplanetario");
+        System.out.println("************************************");
+        System.out.println("\nSelecciona una de las siguientes opciones");
+        System.out.println("\n1. Seleccionar un planeta de destino.");
+        System.out.println("2. Seleccionar una nave espacial.");
+        System.out.println("3. Iniciar la simulación de viaje");
+        System.out.println("4. Salir del programa.");
+        option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+                showPlanets();  //Sujeto a cambios para agregar metodos de planetas, por ejemplo, "seleccionarPlaneta()""
+                break;
+            case 2:
+                chooseStarship(); //Sujeto a cambios para agregar metodos de naves, por ejemplo, "seleccionarNave()""
+                break;
+            case 3:
+                travelSimulation(); 
+                break;
+            case 4:
+                System.out.println("Saliendo del programa...");
+                startSimulation = false;
+                break;
+            default:
+                System.err.println("Opción inválida. Inténtelo de nuevo");
+        }
+
+    }
+
+    //Print y métodos de planetas
+
+    public static void showPlanets() {
+        
+    }
+
+    //Print y métodos de naves
+
     public static void showStarships(){
         System.out.println("Naves disponibles: ");
         for (int i = 0; i < starships.length; i++) {
@@ -24,7 +87,7 @@ public class App {
         }
     }
 
-    public static int chooseStarship(){
+    public static void chooseStarship(){
         showStarships();
 
         int option;
@@ -39,6 +102,29 @@ public class App {
         } while (option < 0 || option > starships.length);
 
         System.out.println("Nave " + starships[option-1] + " seleccionada correctamente.");
-        return option-1;
-    }    
+        chooseStarship = option-1;
+    }
+
+    //Duración del viaje
+
+    public static void travelDuration(double distance, double speed){
+
+        
+    }
+
+    //Método de simulación de viaje
+
+    public static void travelSimulation(){
+        
+    }
+
+    //Método de eventos aleatorios
+
+    public static void randomEvents(){
+
+    }
+
+    
+
+
 }
