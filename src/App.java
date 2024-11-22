43f4bd20b35d218657314193945ff7ad5d41a74d
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2024-11-21 20:49:23 -0500

    Adición de arrays de planetas y naves

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 public class App {
+    
+    public static String[] planets = {"Mercurio", "Venus", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"};
+    public static double[] distances = {91.0, 41.0, 78.0, 628.0, 1275.0, 2723.0, 4351.0};
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
